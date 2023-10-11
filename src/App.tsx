b6fbd94a391d7a40f45056626340f27780ed6c8c
Kin Ishii <kinishii97@gmail.com>
2023-10-12 07:08:39 +0900

    terminando requisitos iniciando estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,108 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
+
+type FormData = {
+  name: string;
+  login: string;
+  password: string;
+  url: string;
+  id?: number;
+};
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [list, setList] = useState<FormData[]>([]);
+  const [showPasswords, setShowPasswords] = useState(false);
+
+  const toggleShowPasswords = () => {
+    setShowPasswords((prevState) => !prevState);
+  };
+
+  const addItems = (item: FormData) => {
+    setList([...list, item]);
+    setShowForm(false);
+    setShowButton(true);
+    console.log(list);
+  };
+
+  const clickShowHandler = () => {
+    setShowForm(true);
+    setShowButton(false);
+  };
+
+  const clickHideHandler = () => {
+    setShowForm(false);
+    setShowButton(true);
+  };
+
+  const deleteItem = (id: number | undefined) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
+
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <Header />
+      {showButton && (
+        <div>
+          <label htmlFor="showPassword">Esconder senhas</label>
+          <input
+            id="showPassword"
+            type="checkbox"
+            checked={showPasswords}
+            onChange={toggleShowPasswords}
+          />
+          <button onClick={clickShowHandler}>Cadastrar nova senha</button>
+        </div>
+      )}
+      <button onClick={clickHideHandler}>Cancelar</button>
+      {showForm && <Form addItems={addItems} />}
+      {list.length > 0 ? (
+        list.map((item) => (
+          <div key={item.id}>
+            <a href={item.url}>{item.name}</a>
+            <p>{item.login}</p>
+            {!showPasswords ? (
+              <p>{item.password}</p>
+            ) : (
+              <p>{"*".repeat(item.password.length)}</p>
+            )}
+            <button
+              data-testid="remove-btn"
+              onClick={() => deleteItem(item.id)}
+            >
+              Apagar
+            </button>
+          </div>
+        ))
+      ) : (
+        <p>Nenhuma senha cadastrada</p>
+      )}
+      {/* {list.length > 0 ? (
+        list.map((item) => {
+          <div key={item.id}>
+            <a href={list.url}>{list.name}</a>
+            <p>{list.login}</p>
+            {showPasswords ? (
+              <p>{item.password}</p>
+            ) : (
+              <p>{"*".repeat(item.password.length)}</p>
+            )}
+            <button
+              data-testid="remove-btn"
+              onClick={() => deleteItem(list.id)}
+            >
+              Apagar
+            </button>
+          </div>;
+        })
+      ) : (
+        <p>Nenhuma senha cadastrada</p>
+      )} */}
+    </>
   );
 }
 
---
+++ src/components/Form.tsx
@@ -0,0 +1,177 @@
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+
+type FormData = {
+  name: string;
+  login: string;
+  password: string;
+  url: string;
+  id?: number;
+};
+
+type FormProps = {
+  addItems: (item: FormData) => void;
+};
+
+function Form({ addItems }: { addItems: FormProps }) {
+  const [isFormComplete, setIsFormComplete] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState({
+    min: false,
+    max: false,
+    numberAndLetter: false,
+    specialCharacter: false,
+  });
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    login: "",
+    password: "",
+    url: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const changeHandler = (value: string, id: string) => {
+    setFormData({ ...formData, [id]: value });
+
+    const isComplete = Object.values(formData).every((item) => item !== "");
+    const isPasswordValid = Object.values(passwordCheck).every(
+      (item) => item === true);
+    if (
+      isComplete && isPasswordValid
+    ) {
+      setIsFormComplete(true);
+    }
+  };
+
+  const passwordChangeHandler = (value: string) => {
+    setFormData({ ...formData, password: value });
+    const newPasswordCheck = {
+      min: value.length > 8,
+      max: value.length < 16,
+      numberAndLetter: /[0-9]/.test(value) && /[a-zA-Z]/.test(value),
+      specialCharacter: /[!@#$%^&*(),.?":{}|<>]/.test(value),
+    };
+    setPasswordCheck(newPasswordCheck);
+  };
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = Math.random();
+
+    const item = { ...formData, id };
+    console.log(item);
+    addItems(item);
+
+    Swal.fire({
+      title: 'Serviço cadastrado com sucesso',
+      timer: 1500,
+      icon: 'success',
+    });
+
+    setFormData({
+      name: "",
+      login: "",
+      password: "",
+      url: "",
+    });
+
+    setIsFormComplete(false);
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div>
+        <p
+          className={
+            passwordCheck.min
+              ? "valid-password-check"
+              : "invalid-password-check"
+          }
+        >
+          Possuir 8 ou mais caracteres
+        </p>
+        <p
+          className={
+            passwordCheck.max
+              ? "valid-password-check"
+              : "invalid-password-check"
+          }
+        >
+          Possuir até 16 caracteres
+        </p>
+        <p
+          className={
+            passwordCheck.numberAndLetter
+              ? "valid-password-check"
+              : "invalid-password-check"
+          }
+        >
+          Possuir letras e números
+        </p>
+        <p
+          className={
+            passwordCheck.specialCharacter
+              ? "valid-password-check"
+              : "invalid-password-check"
+          }
+        >
+          Possuir algum caractere especial
+        </p>
+      </div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Nome do serviço</label>
+        <input
+          required
+          value={formData.name}
+          id="name"
+          type="text"
+          onChange={(e) => changeHandler(e.target.value, e.target.id)}
+        />
+
+        <label htmlFor="login">Login</label>
+        <input
+          required
+          value={formData.login}
+          id="login"
+          type="text"
+          onChange={(e) => changeHandler(e.target.value, e.target.id)}
+        />
+
+        <label htmlFor="password">Senha</label>
+        <input
+          required
+          value={formData.password}
+          id="password"
+          type={showPassword ? "text" : "password"}
+          onChange={(e) => passwordChangeHandler(e.target.value)}
+        />
+        <button
+          data-testid="show-hide-form-password"
+          type="button"
+          onClick={toggleShowPassword}
+        >
+          {showPassword ? "Esconder" : "Mostrar"}
+        </button>
+
+        <label htmlFor="url">URL</label>
+        <input
+          required
+          value={formData.url}
+          id="url"
+          type="text"
+          onChange={(e) => changeHandler(e.target.value, e.target.id)}
+        />
+        <button disabled={!isFormComplete} type="submit">
+          Cadastrar
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Form;
